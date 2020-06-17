6e5cea5d8e39ca86b325382d76c681608051f644
kiwitree <kiwitreekor@gmail.com>
2020-06-17 22:04:03 +0900

    Add: setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='theseed-bot',
+    version='2.3',
+	description='theseed engine api',
+	url='https://github.com/kiwitreekor/theseed-bot',
+	author='kiwitree',
+	author_email='kiwitreekor@gmail.com',
+	packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.7'
+)
