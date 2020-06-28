2bb2ca11931f9758fc6c31f5b82f40d2777a7e64
kiwitree <kiwitreekor@gmail.com>
2020-06-28 20:06:17 +0900

    Add: clearfix, tableofcontents macro

--- theseed_bot/namumark.py
+++ theseed_bot/namumark.py
@@ -767,7 +767,7 @@ class Macro(MarkedText):
     
     name = 'Macro'
     
-    defined_macros = ['date', 'datetime', 'br', 'pagecount', 'include', 'footnote', 'age', 'dday', 'ruby', 'youtube', 'kakaotv', 'nicovideo']
+    defined_macros = ['목차', 'tableofcontents', 'date', 'datetime', 'br', 'pagecount', 'include', 'footnote', 'age', 'dday', 'ruby', 'youtube', 'kakaotv', 'nicovideo', 'clearfix']
     
     def preprocess(self, content, offset):
         match_macro = re.match(r'(.*?)(?:\((.*?)(?<!\\)\))?(?=\])', content[offset:])
